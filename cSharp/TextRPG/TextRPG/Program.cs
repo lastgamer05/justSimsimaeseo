f0ec56087cac4430d9475d29f1c98f007f1683ca
daeun,Kang <kangda0519@gmail.com>
2024-05-25 05:57:48 +0900

    야구

---
+++ cSharp/TextRPG/TextRPG/Program.cs
@@ -0,0 +1,145 @@
+﻿namespace TextRPG
+{
+    internal class Program
+    {
+
+        enum WeaponType
+        {
+            None = 0,
+            Revolver = 1,
+            Shotgun = 2,
+            Ak47 = 3,
+            Sniper = 4,
+        }
+
+        enum GunItem
+        {
+            None = 0,
+            Scope = 1, //사정거리 상승
+            Compensator = 2, //명중률 상승
+
+        }
+
+        enum HumanItem
+        {
+            None = 0,
+
+        }
+
+        struct Player
+        {
+            public int damage; // 한 번 쏠 때 대미지
+            public int mobility; // 이동할 수 있는 거리 (칸)
+            public int range; // 사정거리 (칸)
+            public int accuracy; // 명중률 (%)
+        }
+
+        static WeaponType chooseWeapon()
+        {
+            Console.WriteLine("사용할 총기를 고르시오");
+            Console.WriteLine("1. 리볼버");
+            Console.WriteLine("2. 샷건");
+            Console.WriteLine("3. 소총");
+            Console.WriteLine("4. 저격소총");
+
+            WeaponType weaponType = WeaponType.None;
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    weaponType = WeaponType.Revolver;
+                    break;
+                case "2":
+                    weaponType = WeaponType.Shotgun;
+                    break;
+                case "3":
+                    weaponType = WeaponType.Ak47;
+                    break;
+                case "4":
+                    weaponType = WeaponType.Sniper;
+                    break;
+            }
+
+            return weaponType;
+        }
+
+        static void createPlayer(WeaponType weaponType, out Player player)
+        {
+            switch (weaponType)
+            {
+                case WeaponType.Revolver:
+                    player.damage = 15;
+                    player.mobility = 5;
+                    player.range = 5;
+                    player.accuracy = 70;  // 한 탄창 수도 만들어서 연사 가능하게 했으면 좋겠음
+                    break;                 // 저격은 연발 불가, 소총은 연사 많을수록 정확도 떨어지게
+                case WeaponType.Shotgun:
+                    player.damage = 80;
+                    player.mobility = 3;
+                    player.range = 2;
+                    player.accuracy = 70;
+                    break;
+                case WeaponType.Ak47:
+                    player.damage = 25;
+                    player.mobility = 3;
+                    player.range = 10;
+                    player.accuracy = 60;
+                    break;
+                case WeaponType.Sniper:
+                    player.damage = 80;
+                    player.mobility = 1;
+                    player.range = 15;
+                    player.accuracy = 80;
+                    break;
+                default:
+                    player.damage = 0;
+                    player.mobility = 0;
+                    player.range = 0;
+                    player.accuracy = 0;
+                    break;
+            }
+        }
+
+        static void Move(Player player)
+        {
+            Console.WriteLine("상 [w], 하 [s], 좌 [a], 우 [d] 를 입력하시오 ex) wdd");
+            Console.WriteLine("이동 가능 횟수 : %d", player.mobility);
+
+            string moving = Console.ReadLine();
+
+            while (moving.Length > player.mobility || moving.Length < 1)
+            {
+                Console.WriteLine("이동 가능 횟수 : %d", player.mobility);
+                moving = Console.ReadLine();
+            }
+
+            for (int i = 0; i < moving.Length; i++)
+            {
+                
+            }
+            
+        }
+        static void Game()
+        {
+            Console.WriteLine("0000000000000000000000000"); //25 X 11
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+            Console.WriteLine("0000000000000000000000000");
+        }
+
+        static void Main(string[] args)
+        {
+
+            Game();
+        }
+    }
+}
---
+++ python/text_baseball/main.py
@@ -0,0 +1,133 @@
+import random
+
+
+global strike, ball, out
+strike = 0
+ball = 0
+out = 0
+
+def initCount():
+    global strike, ball
+    strike = 0
+    ball = 0
+
+
+class Pitcher:
+    def __init__(self, ballSpeed, command, ballStrength, hp):
+        self.ballSpeed = ballSpeed # 헛스윙 확률이 높아짐
+        self.command = command # 스트 확률, 헛스윙 확률, 땅볼 확률이 높아짐
+        self.ballStrength = ballStrength # 뜬공 확률 높아짐
+        self.hp = hp # 낮아질수록 위 스탯들 낮아짐
+
+    def pitch(self, course):
+        '''prob_strike = 50 + (self.command / 2.5)
+        prob_ball = 100 - prob_strike
+        prob_deadBall = prob_ball * 0.2
+        prob_wildPitch = prob_ball * 0.1
+
+        rand = random.randrange(100)
+
+        if rand < prob_strike:
+            print("스트라이크")'''
+        courseNum = 0
+        if course == 1: # 상단 스윙
+            courseNum = 1
+        elif course == 2: # 중단 스윙
+            courseNum = 2
+        else: # 하단 스윙
+            courseNum = 3
+        return courseNum
+
+
+
+class Batter:
+    def __init__(self, power, contact, speed, battingEye):
+        self.power = power # 장타, 홈런을 칠 확률이 높아짐
+        self.contact = contact # 스윙 시에 공을 맞출 확률이 높아짐
+        self.speed = speed # 주루 플레이 성공 확률이 높아짐
+        self.battingEye = battingEye # 볼인 공을 골라낼 확률이 높아짐
+
+
+def swing(batter_choice, pitcher_choice, accruracy):
+    global strike, ball, out
+    if batter_choice == pitcher_choice:
+        rand = random.randint(0, 100)
+        if rand < accruracy:
+            print("안타!")
+            initCount()
+        else:
+            print("땅볼 아웃!")
+            initCount()
+            out += 1
+    else:
+        print("헛스윙!")
+        strike += 1
+
+
+
+
+def batterMode(p1, b1):
+    is3Out = False
+    isOnBase = False
+
+    global strike, ball, out
+
+    if strike == 3:
+        out += 1
+    if out == 3:
+        is3Out = True
+
+    while not is3Out and not isOnBase:
+        print()
+        print("{}s {}b {}out".format(strike, ball, out))
+        print("타석에 들어섰다!")
+        print("1. 스윙한다")
+        print("2. 지켜본다")
+        choice = int(input())
+        print()
+
+        if choice == 1:
+            pitcher_choice = random.randint(1, 3)
+            print("1. 상단스윙")
+            print("2. 중단스윙")
+            print("3. 하단스윙")
+            batter_choice = int(input())
+            accuracy = 100 + b1.contact - p1.ballSpeed - p1.command # 안타, 홈런칠 확률
+            swing(batter_choice, pitcher_choice, accuracy)
+    return is3Out
+
+def game():
+    print("1. 타자모드")
+    print("2. 투수모드 (아직 준비중)")
+    choice = int(input())
+    print()
+
+    if choice == 1:
+        pitcher1 = Pitcher(50, 50, 50, 50)
+
+        print("능력치를 설정해 주세요 (합이 200 이하여야 함)")
+        power = int(input("파워 : "))
+        contact = int(input("컨택 : "))
+        speed = int(input("스피드 : "))
+        battingEye = int(input("선구안 : "))
+        statSum = power + contact + speed + battingEye
+
+        while statSum > 200 or statSum < 0:
+            print()
+            print("능력치를 설정해 주세요 (합이 200 이하여야 함)")
+            power = int(input("파워 : "))
+            contact = int(input("컨택 : "))
+            speed = int(input("스피드 : "))
+            battingEye = int(input("선구안 : "))
+            statSum = power + contact + speed + battingEye
+
+        batter1 = Batter(power, contact, speed, battingEye)
+
+        batterMode(pitcher1, batter1)
+
+
+
+
+game()
+
+
